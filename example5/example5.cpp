ccee35eb4c435556cd79ecfdc2882096b565d003
한상윤 <gkstkddb1225@naver.com>
2019-03-25 21:51:16 +0900

    새로운 과제를(example5)를 생성한다.

--- example5/example5.cpp
+++ example5/example5.cpp
@@ -18,75 +18,153 @@ int main()
 	double radian = dir * M_PI / 180.;
 	int count = 10;
 	int by = py;
-	int bx = px - count * sin(radian) - 15;
+	int bx = px - count * sin(radian);
 	int bz = pz + count * cos(radian);
 	locateBlock(goldblock, bx, py, bz);
 	int x = bx, y = py, z = bz;
+	printf("%f", dir);
 
-	for (; x < (bx + 30); x++)
+	if (fabs(sin(radian)) < fabs(cos(radian)))
 	{
-		for (; z < (bz + 15); z++)
+		bx = bx - 15;
+		x = bx;
+		for (; x < (bx + 30); x++)
 		{
-			locateWool(black_wool, x, y, z);
+			for (; y < (by + 15); y++)
+			{
+				locateWool(black_wool, x, y, z);
+			}
+			y = by;
+		}
+		x = bx;
+		y = py;
+		for (; x < (bx + 40); x++)
+		{
+			for (; y < (by + 40); y++)
+			{
+				if (((bx + 7 - x)*(bx + 7 - x) + (by + 7 - y) * (by + 7 - y)) <= 6 * 6 + 1)
+				{
+					locateBlock(goldblock, x, y, z);
+				}
+			}
+
+			y = by;
+		}
+		locateWool(black_wool, bx + 14, by + 6, bz);
+		locateWool(black_wool, bx + 14, by + 7, bz);
+		locateWool(black_wool, bx + 14, by + 8, bz);
+
+		locateWool(black_wool, bx + 13, by + 4, bz);
+		locateWool(black_wool, bx + 13, by + 5, bz);
+		locateWool(black_wool, bx + 13, by + 6, bz);
+		locateWool(black_wool, bx + 13, by + 7, bz);
+		locateWool(black_wool, bx + 13, by + 8, bz);
+		locateWool(black_wool, bx + 13, by + 9, bz);
+		locateWool(black_wool, bx + 13, by + 10, bz);
+
+		locateWool(black_wool, bx + 12, by + 4, bz);
+		locateWool(black_wool, bx + 12, by + 5, bz);
+		locateWool(black_wool, bx + 12, by + 6, bz);
+		locateWool(black_wool, bx + 12, by + 7, bz);
+		locateWool(black_wool, bx + 12, by + 8, bz);
+		locateWool(black_wool, bx + 12, by + 9, bz);
+		locateWool(black_wool, bx + 12, by + 10, bz);
+
+		locateWool(black_wool, bx + 11, by + 4, bz);
+		locateWool(black_wool, bx + 11, by + 5, bz);
+		locateWool(black_wool, bx + 11, by + 6, bz);
+		locateWool(black_wool, bx + 11, by + 7, bz);
+		locateWool(black_wool, bx + 11, by + 8, bz);
+		locateWool(black_wool, bx + 11, by + 9, bz);
+		locateWool(black_wool, bx + 11, by + 10, bz);
+
+		locateWool(black_wool, bx + 10, by + 5, bz);
+		locateWool(black_wool, bx + 10, by + 6, bz);
+		locateWool(black_wool, bx + 10, by + 7, bz);
+		locateWool(black_wool, bx + 10, by + 8, bz);
+		locateWool(black_wool, bx + 10, by + 9, bz);
+
+		locateWool(black_wool, bx + 9, by + 6, bz);
+		locateWool(black_wool, bx + 9, by + 7, bz);
+		locateWool(black_wool, bx + 9, by + 8, bz);
+
+		locateWool(black_wool, bx + 8, by + 7, bz);
+
+		locateBlock(goldblock, bx + 14, by + 7, bz);
+		locateBlock(goldblock, bx + 19, by + 7, bz);
+		locateBlock(goldblock, bx + 24, by + 7, bz);
+		return 0;
+	}
+	if (fabs(sin(radian)) > fabs(cos(radian)))
+	{
+		bz = bz - 15;
+		z = bz;
+		for (; z < (bz + 30); z++)
+		{
+			for (; y < (by + 15); y++)
+			{
+				locateWool(black_wool, x, y, z);
+			}
+			y = by;
 		}
 		z = bz;
-	}
-	x = bx;
-	y = py;
-	z = bz;
-	for (; x < (bx + 40); x++)
-	{
+		y = py;
 		for (; z < (bz + 40); z++)
 		{
-			if (((bx + 7 - x)*(bx + 7 - x) + (bz + 7 - z) * (bz + 7 - z)) <= 6 * 6 + 1)
+			for (; y < (by + 40); y++)
 			{
-				locateBlock(goldblock, x, y, z);
+				if (((bz + 7 - z)*(bz + 7 - z) + (by + 7 - y) * (by + 7 - y)) <= 6 * 6 + 1)
+				{
+					locateBlock(goldblock, x, y, z);
+				}
 			}
+
+			y = by;
 		}
+		locateWool(black_wool, bx, by + 6, bz + 14);
+		locateWool(black_wool, bx, by + 7, bz + 14);
+		locateWool(black_wool, bx, by + 8, bz + 14);
 
-		z = bz;
+		locateWool(black_wool, bx, by + 4, bz + 13);
+		locateWool(black_wool, bx, by + 5, bz + 13);
+		locateWool(black_wool, bx, by + 6, bz + 13);
+		locateWool(black_wool, bx, by + 7, bz + 13);
+		locateWool(black_wool, bx, by + 8, bz + 13);
+		locateWool(black_wool, bx, by + 9, bz + 13);
+		locateWool(black_wool, bx, by + 10, bz + 13);
+
+		locateWool(black_wool, bx, by + 4, bz + 12);
+		locateWool(black_wool, bx, by + 5, bz + 12);
+		locateWool(black_wool, bx, by + 6, bz + 12);
+		locateWool(black_wool, bx, by + 7, bz + 12);
+		locateWool(black_wool, bx, by + 8, bz + 12);
+		locateWool(black_wool, bx, by + 9, bz + 12);
+		locateWool(black_wool, bx, by + 10, bz + 12);
+
+		locateWool(black_wool, bx, by + 4, bz + 11);
+		locateWool(black_wool, bx, by + 5, bz + 11);
+		locateWool(black_wool, bx, by + 6, bz + 11);
+		locateWool(black_wool, bx, by + 7, bz + 11);
+		locateWool(black_wool, bx, by + 8, bz + 11);
+		locateWool(black_wool, bx, by + 9, bz + 11);
+		locateWool(black_wool, bx, by + 10, bz + 11);
+
+		locateWool(black_wool, bx, by + 5, bz + 10);
+		locateWool(black_wool, bx, by + 6, bz + 10);
+		locateWool(black_wool, bx, by + 7, bz + 10);
+		locateWool(black_wool, bx, by + 8, bz + 10);
+		locateWool(black_wool, bx, by + 9, bz + 10);
+
+		locateWool(black_wool, bx, by + 6, bz + 9);
+		locateWool(black_wool, bx, by + 7, bz + 9);
+		locateWool(black_wool, bx, by + 8, bz + 9);
+
+		locateWool(black_wool, bx, by + 7, bz + 8);
+
+		locateBlock(goldblock, bx, by + 7, bz + 14);
+		locateBlock(goldblock, bx, by + 7, bz + 19);
+		locateBlock(goldblock, bx, by + 7, bz + 24);
+		return 0;
 	}
-	locateWool(black_wool, bx + 14, by, bz + 6);
-	locateWool(black_wool, bx + 14, by, bz + 7);
-	locateWool(black_wool, bx + 14, by, bz + 8);
 
-	locateWool(black_wool, bx + 13, by, bz + 4);
-	locateWool(black_wool, bx + 13, by, bz + 5);
-	locateWool(black_wool, bx + 13, by, bz + 6);
-	locateWool(black_wool, bx + 13, by, bz + 7);
-	locateWool(black_wool, bx + 13, by, bz + 8);
-	locateWool(black_wool, bx + 13, by, bz + 9);
-	locateWool(black_wool, bx + 13, by, bz + 10);
-
-	locateWool(black_wool, bx + 12, by, bz + 4);
-	locateWool(black_wool, bx + 12, by, bz + 5);
-	locateWool(black_wool, bx + 12, by, bz + 6);
-	locateWool(black_wool, bx + 12, by, bz + 7);
-	locateWool(black_wool, bx + 12, by, bz + 8);
-	locateWool(black_wool, bx + 12, by, bz + 9);
-	locateWool(black_wool, bx + 12, by, bz + 10);
-
-	locateWool(black_wool, bx + 11, by, bz + 4);
-	locateWool(black_wool, bx + 11, by, bz + 5);
-	locateWool(black_wool, bx + 11, by, bz + 6);
-	locateWool(black_wool, bx + 11, by, bz + 7);
-	locateWool(black_wool, bx + 11, by, bz + 8);
-	locateWool(black_wool, bx + 11, by, bz + 9);
-	locateWool(black_wool, bx + 11, by, bz + 10);
-
-	locateWool(black_wool, bx + 10, by, bz + 5);
-	locateWool(black_wool, bx + 10, by, bz + 6);
-	locateWool(black_wool, bx + 10, by, bz + 7);
-	locateWool(black_wool, bx + 10, by, bz + 8);
-	locateWool(black_wool, bx + 10, by, bz + 9);
-
-	locateWool(black_wool, bx + 9, by, bz + 6);
-	locateWool(black_wool, bx + 9, by, bz + 7);
-	locateWool(black_wool, bx + 9, by, bz + 8);
-
-	locateWool(black_wool, bx + 8, by, bz + 7);
-
-	locateBlock(goldblock, bx + 14, by, bz + 7);
-	locateBlock(goldblock, bx + 19, by, bz + 7);
-	locateBlock(goldblock, bx + 24, by, bz + 7);
 }
